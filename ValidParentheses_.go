8f007de3a04ac87820ed015940986bf714442c99
sungjun-ever <zkfmak10@gmail.com>
2026-01-16 17:30:41 +0900

    use ai...

--- ValidParentheses_.go
+++ ValidParentheses_.go
@@ -1,8 +1,31 @@
 package main
 
-import "container/heap"
-
 // https://leetcode.com/problems/valid-parentheses/description/
 func isValid(s string) bool {
+	pairs := map[byte]byte{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
 
+	stack := []byte{}
+
+	for i := 0; i < len(s); i++ {
+		char := s[i]
+
+		// 닫는 괄호인 경우
+		if open, ok := pairs[char]; ok {
+			// 스택이 비어있거나 짝이 아닌경우
+			if len(stack) == 0 || open != stack[len(stack)-1] {
+				return false
+			}
+
+			// 짝이 맞으면 제거
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, char)
+		}
+	}
+
+	return len(stack) == 0
 }
