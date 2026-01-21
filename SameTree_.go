434c6d3483d13784dba36990b8af9ddeeb221455
sungjun-ever <zkfmak10@gmail.com>
2026-01-21 13:31:21 +0900

    제대로 못품

---
+++ SameTree_.go
@@ -0,0 +1,16 @@
+package main
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p == nil || q == nil || p.Val != q.Val {
+		return false
+	}
+
+	left := isSameTree(p.Left, q.Left)
+	right := isSameTree(p.Right, q.Right)
+
+	return left && right
+}
