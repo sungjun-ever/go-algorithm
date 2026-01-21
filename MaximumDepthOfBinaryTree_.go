21febb2737a736c6683984757c05e5b9d79199b1
sungjun-ever <zkfmak10@gmail.com>
2026-01-21 14:25:52 +0900

    제대로 못품

---
+++ MaximumDepthOfBinaryTree_.go
@@ -0,0 +1,12 @@
+package main
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+
+	return max(leftDepth, rightDepth) + 1
+}
