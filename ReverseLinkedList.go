fd3f96e71d64f9fb2f1f94cc51cab4b2b252a1b6
sungjun-ever <zkfmak10@gmail.com>
2026-02-24 15:43:30 +0900

    흐름 추가

--- ReverseLinkedList.go
+++ ReverseLinkedList.go
@@ -1,5 +1,10 @@
 package main
 
+// prev // curr
+// nil // 1 -> 2 -> 3 -> nil
+// nil <- 1 // 2 -> 3 -> nil
+// nil <- 1 <- 2 // 3 -> nil
+// nil <- 1 <- 2 <- 3 // nil
 func reverseList(head *ListNode) *ListNode {
 	curr := head
 	var prev *ListNode
