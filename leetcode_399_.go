0fd46d3110703bc120cbbc2298d1fad5a0320eef
sungjun-ever <zkfmak10@gmail.com>
2026-03-05 18:06:56 +0900

    leetcode 399

---
+++ leetcode_399_.go
@@ -0,0 +1,57 @@
+package main
+
+func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
+	graph := make(map[string]map[string]float64)
+	for i := 0; i < len(values); i++ {
+		equation := equations[i]
+		val := values[i]
+		x, y := equation[0], equation[1]
+
+		if graph[x] == nil {
+			graph[x] = make(map[string]float64)
+		}
+
+		if graph[y] == nil {
+			graph[y] = make(map[string]float64)
+		}
+
+		graph[x][y] = val
+		graph[y][x] = 1.0 / val
+	}
+
+	var dfs func(curr, target string, visited map[string]bool) float64
+
+	dfs = func(curr, target string, visited map[string]bool) float64 {
+		if curr == target {
+			return 1.0
+		}
+
+		visited[curr] = true
+
+		for neighbor, weight := range graph[curr] {
+			if !visited[neighbor] {
+				result := dfs(neighbor, target, visited)
+
+				if result != -1.0 {
+					return weight * result
+				}
+			}
+		}
+
+		return -1.0
+	}
+
+	answer := make([]float64, len(queries))
+	for i, q := range queries {
+		src, dst := q[0], q[1]
+
+		if graph[src] == nil || graph[dst] == nil {
+			answer[i] = -1.0
+		} else {
+			visited := make(map[string]bool)
+			answer[i] = dfs(src, dst, visited)
+		}
+	}
+
+	return answer
+}
