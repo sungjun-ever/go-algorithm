83bd78385c6aca27c833f0013d00e7fa713a5575
sungjun-ever <zkfmak10@gmail.com>
2026-03-11 16:13:14 +0900

    leetcode 215

---
+++ leetcode_215_.go
@@ -0,0 +1,38 @@
+package main
+
+import "container/heap"
+
+type IntHeap []int
+
+// Len, Less, Swap, Push, Pop
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func findKthLargest(nums []int, k int) int {
+	h := &IntHeap{}
+	heap.Init(h)
+
+	for _, n := range nums {
+		heap.Push(h, n)
+
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+
+	return (*h)[0]
+}
