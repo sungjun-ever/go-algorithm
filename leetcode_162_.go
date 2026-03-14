fa03dd33805d46de5ee59f9428c90fa378362567
sungjun-ever <zkfmak10@gmail.com>
2026-03-14 16:05:11 +0900

    leetcode 162

--- leetcode_162_.go
+++ leetcode_162_.go
@@ -1,1 +1,20 @@
 package main
+
+func findPeakElement(nums []int) int {
+	// 산 정상 숫자
+	// 오른쪽, 왼쪽보다 크면 정상
+	left, right := 0, len(nums)-1
+
+	// left와 right가 만나면 정상
+	for left < right {
+		mid := left + (right-left)/2
+
+		if nums[mid] < nums[mid+1] {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+
+	return left
+}
