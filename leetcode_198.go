170349c616a70fc854fa1aabf1c36fc77baa7795
sungjun-ever <zkfmak10@gmail.com>
2026-03-17 14:21:09 +0900

    leetcode 198 add comments

--- leetcode_198.go
+++ leetcode_198.go
@@ -4,6 +4,9 @@ func rob(nums []int) int {
 	prev2 := 0 // 전전
 	prev1 := 0 // 전
 
+	// 두, 세 번째부터 턴다고 가정했을때
+	// 세 번째는 첫 번째 + 현재, 두 번째는 두 번째
+	// 둘 중 더 큰걸 더해준다
 	for _, n := range nums {
 		curr := max(prev2+n, prev1)
 
