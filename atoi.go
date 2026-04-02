2806878ab63caf3bcdc602fce4ea051e890321fc
sungjun-ever <zkfmak10@gmail.com>
2026-04-02 13:19:37 +0900

    atoi

--- atoi.go
+++ atoi.go
@@ -1,25 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	var input string
 	fmt.Scan(&input)
 	isNegitive := false
 
+	if len(input) == 0 {
+		panic("empty string")
+	}
+
 	if input[0] == '-' {
 		input = input[1:]
 		isNegitive = true
+	} else if input[0] == '+' {
+		input = input[1:]
 	}
 
 	nums := 0
 	for _, s := range input {
-		nums = nums*10 + int(s-'0')
+		if s < '0' || s > '9' {
+			panic("invalid character")
+		}
+
+		digit := int(s - '0')
+
+		if nums > (math.MaxInt32-digit)/10 {
+			panic("overflow")
+		}
+
+		nums = nums*10 + digit
 	}
 
 	if isNegitive {
 		nums = -nums
 	}
-	fmt.Printf("%T", nums)
+
 	fmt.Println(nums)
 }
